790c745b8ccf06edf4367bf071a4cd8d0ce073c1
isanjeev23 <sk315055@gmail.com>
2022-09-30 20:04:00 +0530

    api call implemented

--- main.js
+++ main.js
@@ -4,15 +4,32 @@ let translateBtn = document.querySelector('#translateBTN');
 let clearBtn = document.querySelector('#clearBTN');
 
 
+let url = "https://api.funtranslations.com/translate/minion.json";
+
+function bindKeyValueToUrl(txt) {
+   // console.log(`${url}?text=${txt}`)
+    return `${url}?text=${txt}`;
+}
+
+translateBtn.addEventListener('click', () => {
+    let txtContent = text.value;
+
+    fetch(bindKeyValueToUrl(txtContent))
+    .then(response => response.json())
+    .then(json => {
+         outputText.textContent = json.contents.translated 
+     //    console.log(json);
+        })
+    .catch(error => {
+        alert("Something went wrong see console to know whats the error") ;
+        console.log(err);
+    })
 
 
-translateBtn.addEventListener('click' , () => {
-    let txtContent = text.value ; 
-    outputText.innerHTML = txtContent ;
-    console.log(txtContent) ;
+
 });
 
-clearBtn.addEventListener('click' , () => {
-    text.value = '' ;
+clearBtn.addEventListener('click', () => {
+    text.value = '';
     outputText.textContent = '';
 })
